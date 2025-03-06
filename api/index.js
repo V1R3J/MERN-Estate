bd7d448c70f1b30d6881da7c8e5a53fe0f9be837
V1R3J <virajbhatia1014@gmail.com>
2025-03-07 02:47:22 +0530

    Create and Run the Server

---
+++ api/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(3000, () =>{
+    console.log('Server is Up and Running on Port 3000');
+    }
+);
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
     <header className='bg-green-200 shadow-md '>
         <div className='flex justify-between item-center max-w-6xl mx-auto p-4'>
             <Link to='/'>
-            <h1 className='font-extrabold text-sm sm:text-xl flex flex-wrap'>
+            <h1 className='font-extrabold text-sm sm:text-2xl flex flex-wrap'>
                 <span className='text-emerald-600'>Viraj</span>
                 <span className='text-emerald-400'>RealEstate</span>
             </h1>
@@ -16,7 +16,7 @@ export default function Header() {
                 <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
                 <FaSearch className='text-emerald-500' />
             </form>
-            <ul className='flex gap-4'>
+            <ul className='flex gap-5 items-center'>
                 <Link to='/'>
                 <li className='hidden sm:inline text-emerald-700 hover:underline'>Home</li>
                 </Link>
