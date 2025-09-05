98ce5f4e3f8fcf7230ffb5e477c25f900ba945da
V1R3J <virajbhatia1014@gmail.com>
2025-09-06 01:13:20 +0530

    update the header and made the profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import {FaSearch} from 'react-icons/fa';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import {useSelector} from 'react-redux'; 
+
 
 export default function Header() {
+    const {currentUser} = useSelector(state => state.user)
   return (
     <header className='bg-green-200 shadow-md '>
         <div className='flex justify-between item-center max-w-6xl mx-auto p-4'>
             <Link to='/'>
             <h1 className='font-extrabold text-sm sm:text-2xl flex flex-wrap'>
                 <span className='text-emerald-600'>Viraj</span>
-                <span className='text-emerald-400'>Cipher</span>
+                <span className='text-emerald-400'>Estate</span>
             </h1>
             </Link>
             <form className='bg-green-100 p-3 rounded-full flex items-center'>
@@ -24,8 +27,17 @@ export default function Header() {
                 <Link to='/About'>
                 <li className='hidden sm:inline text-emerald-700 hover:underline'>About</li>
                 </Link>
-                <Link to='Sign-In'>
-                <li className='text-emerald-700 hover:underline'>Sign In</li>
+                
+                <Link to='/profile'>
+                    {currentUser ? (
+                        <img 
+                            className='rounded-full h-10 w-10 object-cover' 
+                            src={currentUser.avatar} 
+                            alt='profile' 
+                        />
+                    ) : (
+                        <li className='text-emerald-700 hover:underline'>Sign In</li>
+                    )}
                 </Link>
             </ul>
         </div>
